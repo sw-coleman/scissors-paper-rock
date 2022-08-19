e475c74bebeee1441c9fac798edb21dd069339fb
Sam <samcoleman98@gmail.com>
2022-08-20 09:06:42 +1000

    Change global variables and add working game function
    Clean up global variable declarations
    Add functions to allow for game to be played, scores to be stored and
    displayed to the player

--- script.js
+++ script.js
@@ -1,42 +1,80 @@
 
+//global variables
+let playerScore = 0;
+let computerScore = 0;
+let drawScore = 0;
+let roundsPlayed = 0;
+let computerSelection = '';
+let playerSelection = '';
 const choices = [
     "rock",
     "paper",
     "scissors"
 ];
-let computerSelection;
-let playerSelectionInput; //To allow for case-insensitive input to be converted to lower case
-let playerSelection;
-let playerScore = 0;
-let computerScore = 0;
-let score = playerScore + ' - ' + computerScore;
-let roundsPlayed = 0;
 
 // Randomly generate a computer selection of rock, paper or scissors.
 function getComputerSelection(){
-    computerSelection = choices[Math.floor(Math.random()*choices.length)]; //Use Math.floor and Math.random to randomly select a value from the choices array
-    return computerSelection;
+    let computerChoice = choices[Math.floor(Math.random()*choices.length)]; //Use Math.floor and Math.random to randomly select a value from the choices array
+    return computerChoice;
 }
 
 // Let user select choice of rock, paper or scissors. 
 function getPlayerSelection(){
-    playerSelectionInput = prompt("Please enter 'scissors', 'paper' or 'rock'.");
-    playerSelection = playerSelectionInput.toLowerCase(); //Make input case-insensitive
+    let playerChoice = prompt("Please enter 'scissors', 'paper' or 'rock'.");
+    playerChoice = playerChoice.toLowerCase(); //Make input case-insensitive
 
-    if (choices.indexOf(playerSelection) !== -1){ //Validate user input against possible chocies
-        return playerSelection;
+    if (choices.indexOf(playerChoice) !== -1){ //Validate user input against possible chocies
+        return playerChoice;
        } else {
-        playerSelectionInput = prompt("Please try again. Enter only 'scissors', 'paper' or 'rock'."); //Error if player does not enter valid selection
-        playerSelection = playerSelectionInput.toLowerCase(); //Make input case-insensitive
-        return playerSelection;
+        playerChoice = prompt("Please try again. Enter only 'scissors', 'paper' or 'rock'."); //Error if player does not enter valid selection
+        playerChoice = playerChoice.toLowerCase(); //Make input case-insensitive
+        return playerChoice;
     }
 }
 
-// Compare player selection with computer selection.
+//Compare player selection with computer selection to determine win, loss or draw. 
+function getWinner(playerSelection, computerSelection) {
+    //Check for draw
+    if (playerSelection === computerSelection) {
+        console.log("It's a draw.")
+        drawScore += 1;
+        return drawScore;
+    //Check for player win
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors' ||
+            playerSelection === 'paper' && computerSelection === 'rock' ||
+            playerSelection === 'scissors' && computerSelection === 'paper'){
+            console.log("You win.")
+            playerScore += 1;
+            return playerScore;
+    //Check for computer win
+    } else if (playerSelection === 'rock' && computerSelection === 'paper' ||
+            playerSelection === 'paper' && computerSelection === 'scissors' ||
+            playerSelection ==='scissors' && computerSelection === 'rock') {
+            console.log("You win.")
+            computerScore += 1;
+            return computerScore;
+    }
+}
+//Log the current score for player wins, computer wins and draws.
+function displayScore(){
+    console.log(`Player: ${playerScore}`);
+    console.log(`Computer: ${computerScore}`);
+    console.log(`Draws: ${drawScore}`);
+}
+//Display the player selection against the computer selection.
+function displaySelection(playerSelection, computerSelection){
+    console.log(`Player: ${playerSelection}`);
+    console.log(`Computer: ${computerSelection}`);
+}
 
-// Generate player's result (win or loss) based upon player's selection against computer selection
-
-// Print result to player, store result in variable to keep score
+//Function to play round. Calls on the functions to allow player/computer to select, compare the results and display/update the score after each round played .
+function playRound(){
+    playerSelection = getPlayerSelection();
+    computerSelection = getComputerSelection();
+    getWinner(playerSelection, computerSelection);
+    displaySelection(playerSelection, computerSelection);
+    displayScore();
+}
 
 // Loop to play until one side (player vs computer) reaches 3 wins
 
