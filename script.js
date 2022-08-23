f4dd7ac73897ad4bd28accb09bbf4f3154f675ba
Sam Coleman <Sam@192-168-1-112.tpgi.com.au>
2022-08-23 16:37:05 +1000

    Add score and result display and option to play again once first to five is achieved
    Add score that updates following each round
    Add result of each round
    Add final result and disable buttons once a score of five is reached
    Add play again button which reloads page.

--- script.js
+++ script.js
@@ -13,19 +13,18 @@ function getComputerSelection(){
     let computerChoice = choices[Math.floor(Math.random()*choices.length)]; //Use Math.floor and Math.random to randomly select a value from the choices array
     return computerChoice;
 }
-
-//Assign selections for event listener. Need to find way to condense all three options into one function, if possible.
+//Assign selections for the event listener. Need to find way to condense all three options into one function if possible.
 function assignScissors() {
     playerSelection = 'scissors';
-    return playerSelection;
+    playRound(playerSelection);
 }
 function assignPaper() {
     playerSelection = 'paper';
-    return playerSelection;
+    playRound(playerSelection);
 }
 function assignRock() {
     playerSelection = 'rock';
-    return playerSelection;
+    playRound(playerSelection);
 }
 
 //Event listeners for buttons, to assign player selection upon click of button.
@@ -37,70 +36,103 @@ paperBtn.addEventListener("click", assignPaper);
 
 const rockBtn = document.querySelector(".rock")
 rockBtn.addEventListener("click", assignRock);
-
-// Let user select choice of rock, paper or scissors. 
-//function getPlayerSelection(){
-//    let playerChoice = prompt("Please enter 'scissors', 'paper' or 'rock'.");
-//    playerChoice = playerChoice.toLowerCase(); //Make input case-insensitive
-//
-//    if (choices.indexOf(playerChoice) !== -1){ //Validate user input against possible chocies
-//        return playerChoice;
-//       } else {
-//        playerChoice = prompt("Please try again. Enter only 'scissors', 'paper' or 'rock'."); //Error if player does not enter valid selection
-//        playerChoice = playerChoice.toLowerCase(); //Make input case-insensitive
-//        return playerChoice;
-//    }
-//}
+//Functions to disable buttons after game.
+function removeScissorsBtn(){
+    scissorsBtn.removeEventListener("click", assignScissors);
+}
+function removePaperBtn(){
+    paperBtn.removeEventListener("click", assignPaper);
+}
+function removeRockBtn(){
+    rockBtn.removeEventListener("click", assignRock);
+}
 
 //Compare player selection with computer selection to determine win, loss or draw. 
 function getWinner(playerSelection, computerSelection) {
     //Check for draw
     if (playerSelection === computerSelection) {
-        console.log("It's a draw.")
+        const resultDisplay = document.querySelector('.result');
+        resultDisplay.textContent = `It's a draw.`
         drawScore += 1;
         return drawScore;
     //Check for player win
     } else if (playerSelection === 'rock' && computerSelection === 'scissors' ||
             playerSelection === 'paper' && computerSelection === 'rock' ||
             playerSelection === 'scissors' && computerSelection === 'paper'){
-            console.log("You win.")
+            const resultDisplay = document.querySelector('.result');
+            resultDisplay.textContent = `You win the round.`
             playerScore += 1;
             return playerScore;
     //Check for computer win
     } else if (playerSelection === 'rock' && computerSelection === 'paper' ||
             playerSelection === 'paper' && computerSelection === 'scissors' ||
             playerSelection ==='scissors' && computerSelection === 'rock') {
-            console.log("You win.")
+            const resultDisplay = document.querySelector('.result');
+            resultDisplay.textContent = `You lose the round.`
             computerScore += 1;
             return computerScore;
     }
 }
 //Log the current score for player wins, computer wins and draws.
 function displayScore(){
-    console.log(`Player: ${playerScore}`);
-    console.log(`Computer: ${computerScore}`);
-    console.log(`Draws: ${drawScore}`);
-    console.log(`--- Rounds played: ${roundsPlayed} ---`);
+    const scoreTrack = document.querySelector('.score-track');
+    scoreTrack.textContent = 
+        `Player: ${playerScore}\n
+        Computer: ${computerScore}\n
+        Draws: ${drawScore}\n
+        Rounds Played: ${roundsPlayed}`;
 }
+
 //Display the player selection against the computer selection.
 function displaySelection(playerSelection, computerSelection){
-    console.log(`Player: ${playerSelection}`);
-    console.log(`Computer: ${computerSelection}`);
+    const selections = document.querySelector('.selections');
+    selections.textContent = `Player: ${playerSelection} Computer: ${computerSelection}`;
 }
+
+//function to create first to five format.
+function firstToFive(){
+    if (playerScore == 5){
+        const gameResult = document.querySelector('.game-result');
+        gameResult.textContent = `You win! Final score: ${playerScore} - ${computerScore}.`
+        restartGame();
+    } else if (computerScore == 5){
+        const gameResult = document.querySelector('.game-result');
+        gameResult.textContent = `You lose! Final score: ${computerScore} - ${playerScore}.`
+        restartGame();
+    }
+}
+
 //Function to track total rounds played
 function gameRounds(){
     roundsPlayed++;
 }
 
 //Function to play round. Calls on the functions to allow player/computer to select, compare the results and display/update the score after each round played .
-function playRound(){
-    playerSelection = getPlayerSelection();
+function playRound(playerSelection){
     computerSelection = getComputerSelection();
     getWinner(playerSelection, computerSelection);
     displaySelection(playerSelection, computerSelection);
     gameRounds();
     displayScore();
+    firstToFive();
 }
+
+//Function to make button appear to restart game once best of 5.
+function restartGame(){
+    const restartBtn = document.createElement('button');
+    document.body.append(restartBtn);
+    restartBtn.textContent = 'Play again';
+    restartBtn.addEventListener('click', function(){
+        window.location.reload()});
+    playerScore = 0;
+    computerScore = 0;
+    drawScore = 0;
+    roundsPlayed = 0;
+    removeScissorsBtn();
+    removePaperBtn();
+    removeRockBtn();
+}
+
  //Loop to play until one side (player vs computer) reaches 5 wins, and display final results 
  //function game(){
  //  while (playerScore < 5 || computerScore < 5) {
